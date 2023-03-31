2a93abbe74f50a0ba3b16a394ac366fcb467071d
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-03-31 15:49:55 +0100

    d290128f-0503-496f-94ad-70e495b5e511

--- src/arm/deploy.sh
+++ src/arm/deploy.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 set -e
 
-######################################
-## Define Variables _ UPDATE VALUES
-BASE_NAME="<TBD>"
-LOCATION="northeurope"
-######################################
+echo "Parameters..."
+echo "Resource Group -> $1";
+echo "Deployment Name -> $2";
+echo "Base Name -> $3";
+echo "Workspace -> $4";
 
-## Resource Group & Deployment
-RESOURCE_GROUP_NAME=$BASE_NAME-rg
-DEPLOYMENT_NAME=$BASE_NAME-deployment-$(date +%s)
+RESOURCE_GROUP_NAME=$1
+DEPLOYMENT_NAME=$2
+BASE_NAME=$3
 
-echo !!!
+# workaround to get bicep to work with azure cli github action
+az config set bicep.use_binary_from_path=false
+az bicep install
+
+result=$(az deployment group create \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $DEPLOYMENT_NAME \
+    --template-file $WORKSPACE/main.bicep \
+    --parameters \
+    baseName=$BASE_NAME \
+    -o json \
+    --query properties.outputs.result.value | jq -r . >$WORKSPACE/outputs.json)
+
+echo "Finshed."
